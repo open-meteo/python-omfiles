99fc5bd354b791cb6b74a3c576bbc7b95c5a3d3f
terraputix <terraputix@mailbox.org>
2025-05-06 14:21:35 +0200

    remove obsolete files

--- python/omfilesrspy/__init__.py
+++
@@ -1,5 +0,0 @@
-# import the contents of the Rust library into the Python extension
-from . import types
-from .omfilesrspy import OmFilePyFsSpecReader, OmFilePyReader, OmFilePyWriter
-
-__all__ = ["OmFilePyFsSpecReader", "OmFilePyReader", "OmFilePyWriter", "types"]
