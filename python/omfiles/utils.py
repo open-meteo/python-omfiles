4fe96589c8e495c4a39d79f8fd20bd96f45189b4
terraputix <terraputix@mailbox.org>
2025-06-26 09:15:02 +0200

    fix typing syntax for older python versions

--- python/omfiles/utils.py
+++ python/omfiles/utils.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import numpy as np
 
 from omfiles.types import ArrayType
@@ -24,5 +26,5 @@ def _modulo_positive(value: int, modulo: int) -> int:
     return ((value % modulo) + modulo) % modulo
 
 
-def _normalize_longitude(lon: ArrayType | float) -> ArrayType | float:
+def _normalize_longitude(lon: Union[ArrayType, float]) -> Union[ArrayType, float]:
     return ((lon + 180.0) % 360.0) - 180.0
