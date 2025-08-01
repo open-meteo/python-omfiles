0f62352f7a02c68b6aca3b7b8bb9ba2ddc9fee41
terraputix <terraputix@mailbox.org>
2025-08-01 14:53:34 +0200

    optional import

--- python/omfiles/__init__.py
+++ python/omfiles/__init__.py
@@ -1,14 +1,20 @@
 """Provides classes and utilities for reading, writing, and manipulating OM files."""
 
-from . import omfiles_zarr_codecs, types, xarray_backend
+from . import types, xarray_backend
 from .omfiles import OmFilePyReader, OmFilePyReaderAsync, OmFilePyWriter, OmVariable
 
 __all__ = [
     "OmFilePyReader",
     "OmFilePyReaderAsync",
     "OmFilePyWriter",
-    "omfiles_zarr_codecs",
     "OmVariable",
     "xarray_backend",
     "types",
 ]
+
+try:
+    from . import omfiles_zarr_codecs
+
+    __all__.append("omfiles_zarr_codecs")
+except ImportError:
+    pass
