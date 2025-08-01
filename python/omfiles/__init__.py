b238e74f0fda68ab92fdbfa2e2dde95dfc33886c
terraputix <terraputix@mailbox.org>
2025-08-01 19:35:03 +0200

    make fsspec, s3fs and xarray optional dependencies

--- python/omfiles/__init__.py
+++ python/omfiles/__init__.py
@@ -1,6 +1,12 @@
 """Provides classes and utilities for reading, writing, and manipulating OM files."""
 
-from . import types, xarray_backend
+from . import types
 from .omfiles import OmFilePyReader, OmFilePyReaderAsync, OmFilePyWriter, OmVariable
 
-__all__ = ["OmFilePyReader", "OmFilePyReaderAsync", "OmFilePyWriter", "OmVariable", "xarray_backend", "types"]
+__all__ = [
+    "OmFilePyReader",
+    "OmFilePyReaderAsync",
+    "OmFilePyWriter",
+    "OmVariable",
+    "types",
+]
