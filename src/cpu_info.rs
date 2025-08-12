9130f45ae89f5ace223f52cd8357c34ad3fb52db
terraputix <terraputix@mailbox.org>
2025-08-12 14:45:58 +0200

    fix arch check

--- src/cpu_info.rs
+++ src/cpu_info.rs
@@ -7,7 +7,7 @@ use pyo3_stub_gen::derive::gen_stub_pyfunction;
 pub fn _check_cpu_features() -> PyResult<()> {
     #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
     {
-        if !std::is_x86_feature_detected!("ssse3") {
+        if !std::arch::is_x86_feature_detected!("ssse3") {
             return Err(pyo3::exceptions::PyRuntimeError::new_err(
                 "This library requires SSSE3 support on x86 CPUs.",
             ));
@@ -15,7 +15,7 @@ pub fn _check_cpu_features() -> PyResult<()> {
     }
     #[cfg(any(target_arch = "arm", target_arch = "aarch64"))]
     {
-        if !std::is_aarch64_feature_detected!("neon") {
+        if !std::arch::is_aarch64_feature_detected!("neon") {
             return Err(pyo3::exceptions::PyRuntimeError::new_err(
                 "This library requires NEON support on ARM CPUs.",
             ));
