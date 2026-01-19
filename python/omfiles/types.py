af5ba2f2245cbc8ad84d66c19fda1769700bdf0f
terraputix <terraputix@mailbox.org>
2026-01-19 10:03:15 +0100

    use type changes from other branch

--- python/omfiles/types.py
+++ python/omfiles/types.py
@@ -4,9 +4,12 @@ try:
     from types import EllipsisType
 except ImportError:
     EllipsisType = type(Ellipsis)
-from typing import Tuple, Union
+from typing import NamedTuple, Tuple, Union
 
 # This is from https://github.com/zarr-developers/zarr-python/blob/main/src/zarr/core/indexing.py#L38C1-L40C87
 
 BasicSelector = Union[int, slice, EllipsisType]
 BasicSelection = Union[BasicSelector, Tuple[Union[int, slice, EllipsisType], ...]]
+
+XYIndex = NamedTuple("XYIndex", [("x", int), ("y", int)])
+LatLon = NamedTuple("LatLon", [("lat", float), ("lon", float)])
